87d8d0b6df7c8ee4755b3cdfef98d107b0c50558
Nipun Batra <nipunb@iiitd.ac.in>
2017-05-22 16:57:11 +0530

    speedier now

--- aaai/harness_custom_tensor.py
+++ aaai/harness_custom_tensor.py
@@ -15,22 +15,21 @@ for appliance in APPLIANCES[:1]:
 
 			for case in range(2, 3):
 				for a in range(1, 2):
-					for home in appliance_df.index:
-						OFILE = "%s/%s_%d.out" % (SLURM_OUT, appliance, home)
-						EFILE = "%s/%s_%d.err" % (SLURM_OUT, appliance, home)
-						SLURM_SCRIPT = "%s_%d.pbs" % (appliance, home)
-						CMD = 'python tensor_fact_custom_harness.py %s %d %d %s %s' % (appliance, case, a, cost, all_features)
-						lines = []
-						lines.append("#!/bin/sh\n")
-						lines.append('#SBATCH --time=0-01:0:00\n')
-						lines.append('#SBATCH --mem=16\n')
-						lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
-						lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
-						lines.append(CMD + '\n')
+					OFILE = "%s/%s_%d.out" % (SLURM_OUT, appliance, a)
+					EFILE = "%s/%s_%d.err" % (SLURM_OUT, appliance, a)
+					SLURM_SCRIPT = "%s_%d.pbs" % (appliance, a)
+					CMD = 'python tensor_fact_custom_harness.py %s %d %d %s %s' % (appliance, case, a, cost, all_features)
+					lines = []
+					lines.append("#!/bin/sh\n")
+					lines.append('#SBATCH --time=0-01:0:00\n')
+					lines.append('#SBATCH --mem=16\n')
+					lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
+					lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
+					lines.append(CMD + '\n')
 
-						with open(SLURM_SCRIPT, 'w') as f:
-							f.writelines(lines)
-						command = ['sbatch', SLURM_SCRIPT]
-						time.sleep(2)
-						print Popen(command)
-					print "Now sleeping.."
+					with open(SLURM_SCRIPT, 'w') as f:
+						f.writelines(lines)
+					command = ['sbatch', SLURM_SCRIPT]
+					time.sleep(2)
+					Popen(command)
+					print appliance, cost, all_features, a
