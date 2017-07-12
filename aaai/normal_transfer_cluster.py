54e32e258da0ac260dd55434fe9f543d3bc49534
Yiling <yj9xs@virginia.edu>
2017-07-12 12:20:59 -0400

    cluster

---
+++ aaai/normal_transfer_cluster.py
@@ -0,0 +1,136 @@
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+from create_matrix import *
+import os
+import sys
+from tensor_custom_core import *
+from create_matrix import *
+from tensor_custom_core import *
+from degree_days import dds
+from sklearn.model_selection import train_test_split, KFold
+from common import compute_rmse_fraction
+from common import compute_rmse
+import pickle
+
+
+def un_normalize(x, maximum, minimum):
+    return (maximum - minimum) * x + minimum
+
+def get_tensor(df, dfc):
+    start, stop = 1, 13
+    energy_cols = np.array(
+        [['%s_%d' % (appliance, month) for month in range(start, stop)] for appliance in APPLIANCES_ORDER]).flatten()
+
+    static_cols = ['area', 'total_occupants', 'num_rooms']
+    static_df = df[static_cols]
+    static_df = static_df.div(static_df.max())
+    weather_values = np.array(dds[2014][region][start - 1:stop - 1]).reshape(-1, 1)
+
+    dfc = df.copy()
+
+    df = dfc[energy_cols]
+    col_max = df.max().max()
+    col_min = df.min().min()
+    # df = (1.0 * (df - col_min)) / (col_max - col_min)
+    tensor = df.values.reshape((len(df), 7, stop - start))
+    M, N, O = tensor.shape
+    return tensor
+
+def save_obj(obj, name ):
+    with open(os.path.expanduser('~/git/'+ name + '.pkl'), 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(name ):
+    with open(os.path.expanduser('~/git/' + name + '.pkl'), 'rb') as f:
+        return pickle.load(f)
+
+
+appliance_index = {appliance: APPLIANCES_ORDER.index(appliance) for appliance in APPLIANCES_ORDER}
+APPLIANCES = ['fridge', 'hvac', 'wm', 'mw', 'oven', 'dw']
+year = 2014
+
+region = "SanDiego"
+sd_df, sd_dfc = create_matrix_single_region("SanDiego", year)
+sd_tensor = get_tensor(sd_df, sd_dfc)
+region = "Austin"
+au_df, au_dfc = create_matrix_single_region("Austin", year)
+au_tensor = get_tensor(au_df, au_dfc)
+
+pred = {}
+n_splits = 10
+case = 2
+a = 2
+b = 3
+num_seed = 2
+cost = 'abs'
+
+for method in ['normal', 'transfer']:
+    pred[method] = {}
+    for cost in ['abs', 'rel']:
+        pred[method][cost] = {}
+        for iterations in [2, 10]:
+            pred[method][cost][iterations] = {}
+            for random_seed in range(num_seed):
+                pred[method][cost][iterations][random_seed] = {}
+                for appliance in APPLIANCES_ORDER:
+                    pred[method][cost][iterations][random_seed][appliance] = {f:[] for f in range(10, 110, 10)}
+                
+kf = KFold(n_splits = n_splits)
+for method in ['normal', 'transfer']:
+    print "method: ", method
+    
+    for cost in ['abs', 'rel']:
+        print "cost: ", cost
+
+        for iterations in [2, 10]:
+            print "iterations: ", iterations
+
+            for random_seed in range(num_seed):
+                print "random seed: ", random_seed
+
+                for train_percentage in range(10, 110, 10):
+                    print "training percentage: ", train_percentage
+                    rd = 0
+
+                    for train_max, test in kf.split(sd_df):
+                        print "round: ", rd
+                        rd += 1
+
+                        num_train = int((train_percentage*len(train_max)/100)+0.5)
+                        num_test = len(test)
+
+                        # get the random training data from train_max based on then random seed
+                        if train_percentage==100:
+                            train = train_max
+                        else:
+                            train, _ = train_test_split(train_max, train_size = train_percentage/100.0, random_state=random_seed)
+
+                        # get the index of training and testing data
+                        train_ix = sd_df.index[train]
+                        test_ix = sd_df.index[test]
+
+                        # create the tensor
+                        train_test_ix = np.concatenate([test_ix, train_ix])
+                        df_t, dfc_t = sd_df.ix[train_test_ix], sd_dfc.ix[train_test_ix]
+                        tensor = get_tensor(df_t, dfc_t)
+                        tensor_copy = tensor.copy()
+                        # set the appliance consumption to be missing for testing data
+                        tensor_copy[:num_test, 1:, :] = np.NaN
+                        
+                        if method == "transfer":
+                            # transfer learning
+                            H_a, A_a, T_a = learn_HAT(case, au_tensor, a, b, num_iter=iterations, lr=0.1, dis=False, 
+                                                      cost_function=cost, T_known=np.ones(12).reshape(-1, 1))
+                            H, A, T = learn_HAT(case, tensor_copy, a, b, num_iter=iterations, lr=0.1, dis=False, 
+                                                cost_function=cost, A_known = A_a, T_known=np.ones(12).reshape(-1, 1))
+                        else:
+                            # normal learning
+                            H, A, T = learn_HAT(case, tensor_copy, a, b, num_iter=iterations, lr=0.1, dis=False, cost_function=cost, T_known=np.ones(12).reshape(-1, 1))
+
+                        # get the prediction
+                        HAT = multiply_case(H, A, T, case)
+                        for appliance in APPLIANCES_ORDER:
+                            pred[method][cost][iterations][random_seed][appliance][train_percentage].append(pd.DataFrame(HAT[:num_test, appliance_index[appliance], :], index=test_ix))
+
+# pred.to_pickle("~/git/pred.pkl") 
+save_obj(pred, "pred")         
---
+++ aaai/normal_transfer_learning_cluster_script.py
@@ -0,0 +1,52 @@
+from create_matrix import create_matrix_region_appliance_year
+from subprocess import Popen
+import os
+# region = "Austin"
+# year = 2014
+import delegator
+
+# Enter your username on the cluster
+username = 'yj9xs'
+
+# Location of .out and .err files
+SLURM_OUT = "~/slurm_out"
+
+# Create the SLURM out directory if it does not exist
+if not os.path.exists(SLURM_OUT):
+	os.makedirs(SLURM_OUT)
+
+# Max. num running processes you want. This is to prevent hogging the cluster
+MAX_NUM_MY_JOBS = 140
+# Delay between jobs when we exceed the max. number of jobs we want on the cluster
+DELAY_NUM_JOBS_EXCEEDED = 10
+import time
+
+
+
+CMD = 'python normal_transfer_cluster.py'
+
+
+
+# for cost in ['abs']:
+# 	for case in range(2, 3):
+# 		for a in range(1, 11):
+# 			OFILE = "%s/%s_%d_%d.out" % (SLURM_OUT,  cost[0],  case, a)
+# 			EFILE = "%s/%s_%d_%d.err" % (SLURM_OUT, cost[0], case, a)
+# 			SLURM_SCRIPT = "%s_%d_%d.pbs" % (cost[0],  case, a)
+# 			CMD = 'python tensor_fact_custom_all_appliances_harness.py %d %d %s' % (case, a, cost)
+# 			lines = []
+# 			lines.append("#!/bin/sh\n")
+# 			lines.append('#SBATCH --time=1-16:0:00\n')
+# 			lines.append('#SBATCH --mem=16\n')
+# 			lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
+# 			lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
+# 			lines.append(CMD + '\n')
+
+# 			with open(SLURM_SCRIPT, 'w') as f:
+# 				f.writelines(lines)
+# 			command = ['sbatch', SLURM_SCRIPT]
+# 			while len(delegator.run('squeue -u %s' % username).out.split("\n")) > MAX_NUM_MY_JOBS + 2:
+# 				time.sleep(DELAY_NUM_JOBS_EXCEEDED)
+
+# 			delegator.run(command, block=False)
+# 			print SLURM_SCRIPT
