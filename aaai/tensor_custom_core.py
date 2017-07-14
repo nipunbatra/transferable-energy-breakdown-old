c228c4ec689f8f6d486b9e62617cf2a5e614b0fa
Nipun Batra <nipunb@iiitd.ac.in>
2017-07-14 18:03:45 +0530

    Aded

--- aaai/tensor_custom_core.py
+++ aaai/tensor_custom_core.py
@@ -35,6 +35,64 @@ def set_known(A, W):
 	return A
 
 
+def learn_HAT_adagrad(case, E_np_masked, a, b, num_iter=2000, lr=0.1, dis=False, cost_function='abs', H_known=None,
+                      A_known=None, T_known=None, random_seed=0, eps=1e-8):
+	np.random.seed(random_seed)
+	if cost_function == 'abs':
+		cost = cost_abs
+	else:
+		cost = cost_rel
+	mg = multigrad(cost, argnums=[0, 1, 2])
+
+	params = {}
+	params['M'], params['N'], params['O'] = E_np_masked.shape
+	params['a'] = a
+	params['b'] = b
+	H_dim_chars = list(cases[case]['HA'].split(",")[0].strip())
+	H_dim = tuple(params[x] for x in H_dim_chars)
+	A_dim_chars = list(cases[case]['HA'].split(",")[1].split("-")[0].strip())
+	A_dim = tuple(params[x] for x in A_dim_chars)
+	T_dim_chars = list(cases[case]['HAT'].split(",")[1].split("-")[0].strip())
+	T_dim = tuple(params[x] for x in T_dim_chars)
+	H = np.random.rand(*H_dim)
+
+	A = np.random.rand(*A_dim)
+	T = np.random.rand(*T_dim)
+
+	sum_square_gradients_H = np.zeros_like(H)
+	sum_square_gradients_A = np.zeros_like(A)
+	sum_square_gradients_T = np.zeros_like(T)
+
+	# GD procedure
+	for i in range(num_iter):
+		del_h, del_a, del_t = mg(H, A, T, E_np_masked, case)
+		sum_square_gradients_H += eps + np.square(del_h)
+		sum_square_gradients_A += eps + np.square(del_a)
+		sum_square_gradients_T += eps + np.square(del_t)
+
+		lr_h = np.divide(lr, np.sqrt(sum_square_gradients_H))
+		lr_a = np.divide(lr, np.sqrt(sum_square_gradients_A))
+		lr_t = np.divide(lr, np.sqrt(sum_square_gradients_T))
+
+		H -= lr_h * del_h
+		A -= lr_a * del_a
+		T -= lr_t * del_t
+		# Projection to known values
+		if H_known is not None:
+			H = set_known(H, H_known)
+		if A_known is not None:
+			A = set_known(A, A_known)
+		if T_known is not None:
+			T = set_known(T, T_known)
+		# Projection to non-negative space
+		H[H < 0] = 0
+		A[A < 0] = 0
+		T[T < 0] = 0
+		if i % 500 == 0:
+			if dis:
+				print(cost(H, A, T, E_np_masked, case))
+	return H, A, T
+
 def learn_HAT(case, E_np_masked, a, b, num_iter=2000, lr=0.1, dis=False, cost_function='abs', H_known=None,
               A_known=None, T_known=None, random_seed=0, random_mul_constant=1,
               random_add_constant=0):
