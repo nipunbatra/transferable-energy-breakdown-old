6db36dca8f292769a42b4c399a1f665cd0ecce3b
Yiling <yj9xs@virginia.edu>
2017-08-02 20:30:40 -0400

    leaveoneout

--- aaai/leave_one_out.py
+++ aaai/leave_one_out.py
@@ -19,33 +19,33 @@ a=2
 cost='abs'
 
 def un_normalize(x, maximum, minimum):
-    return (maximum - minimum) * x + minimum
+	return (maximum - minimum) * x + minimum
 
 def get_tensor(df, dfc):
-    start, stop = 1, 13
-    energy_cols = np.array(
-        [['%s_%d' % (appliance, month) for month in range(start, stop)] for appliance in APPLIANCES_ORDER]).flatten()
+	start, stop = 1, 13
+	energy_cols = np.array(
+		[['%s_%d' % (appliance, month) for month in range(start, stop)] for appliance in APPLIANCES_ORDER]).flatten()
 
-    static_cols = ['area', 'total_occupants', 'num_rooms']
-    static_df = df[static_cols]
-    static_df = static_df.div(static_df.max())
-    weather_values = np.array(dds[2014][region][start - 1:stop - 1]).reshape(-1, 1)
+	static_cols = ['area', 'total_occupants', 'num_rooms']
+	static_df = df[static_cols]
+	static_df = static_df.div(static_df.max())
+	weather_values = np.array(dds[2014][region][start - 1:stop - 1]).reshape(-1, 1)
 
-    dfc = df.copy()
+	dfc = df.copy()
 
-    df = dfc[energy_cols]
-    col_max = df.max().max()
-    col_min = df.min().min()
-    # df = (1.0 * (df - col_min)) / (col_max - col_min)
-    tensor = df.values.reshape((len(df), 7, stop - start))
-    M, N, O = tensor.shape
-    return tensor
+	df = dfc[energy_cols]
+	col_max = df.max().max()
+	col_min = df.min().min()
+	# df = (1.0 * (df - col_min)) / (col_max - col_min)
+	tensor = df.values.reshape((len(df), 7, stop - start))
+	M, N, O = tensor.shape
+	return tensor
 
 def save_obj(obj, name ):
-    with open(os.path.expanduser('~/git/'+ name + '.pkl'), 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+	with open(os.path.expanduser('~/git/'+ name + '.pkl'), 'wb') as f:
+		pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-
+region = 'Austin'
 au_df, au_dfc = create_matrix_single_region("Austin", year)
 au_tensor = get_tensor(au_df, au_dfc)
 static_au = au_df[['area','total_occupants','num_rooms']].copy()
@@ -53,10 +53,10 @@ static_au['area'] = static_au['area'].div(4000)
 static_au['total_occupants'] = static_au['total_occupants'].div(8)
 static_au['num_rooms'] = static_au['num_rooms'].div(8)
 static_au = static_au.values
-
-df, dfc = create_matrix_single_region("SanDiego", year)
-tensor = get_tensor(df, dfc)
-static_sd = df[['area','total_occupants','num_rooms']].copy()
+region = 'SanDiego'
+sd_df, sd_dfc = create_matrix_single_region("SanDiego", year)
+sd_tensor = get_tensor(sd_df, sd_dfc)
+static_sd = sd_df[['area','total_occupants','num_rooms']].copy()
 static_sd['area'] = static_sd['area'].div(4000)
 static_sd['total_occupants'] = static_sd['total_occupants'].div(8)
 static_sd['num_rooms'] = static_sd['num_rooms'].div(8)
@@ -72,11 +72,11 @@ algo = 'adagrad'
 
 
 if static_fac == 'None':
-    H_known_Au = None
-    H_known_Sd = None
+	H_known_Au = None
+	H_known_Sd = None
 else:
-    H_known_Au = static_au
-    H_known_Sd = static_sd
+	H_known_Au = static_au
+	H_known_Sd = static_sd
 
 b = 3
 if algo == 'adagrad':
@@ -99,24 +99,33 @@ else:
 print (static_fac, algo, k)
 
 ## leave one cell out
-m,n,k = sd_tensor.shape
-pred_cell = np.empty[m,n]
+m,n,t = sd_tensor.shape
+pred_cell = np.empty([m,n])
 for i in range(m):
-    for j in range(1,n):
-        print i, j, k
-        tensor_copy = sd_tensor.copy()
-        tensor_copy[i, j, k] = np.NaN
-        
-	
-	if algo == 'adagrad':
-		cost = 'l21'
-		if static_fac == 'static':
-			
-        H, A, T = learn_HAT(case, tensor_copy, a, b, num_iter=2000, lr=0.1, dis=False, cost_function=cost, A_known = A_au, T_known=np.ones(12).reshape(-1, 1))
-        prediction = multiply_case(H, A, T, case)
-        
-        pred_cell[i][j] = prediction[i][j]
+	for j in range(1,n):
+		print i, j, k
+		tensor_copy = sd_tensor.copy()
+		tensor_copy[i, j, k] = np.NaN
+		
+		if algo == 'adagrad':
+			cost = 'l21'
+			if static_fac == 'static':
+				a = 5
+				H_sd, A_sd, T_sd, Hs, As, Ts, HATs, costs = learn_HAT_adagrad(case, tensor_copy, a, b, num_iter=2000, lr=0.1, dis=False, cost_function=cost, H_known = H_known_Sd, A_known = A_au, T_known=np.ones(12).reshape(-1, 1))
+			else:
+				a = 2
+				H_sd, A_sd, T_sd, Hs, As, Ts, HATs, costs = learn_HAT_adagrad(case, tensor_copy, a, b, num_iter=2000, lr=0.1, dis=False, cost_function=cost, A_known = A_au, T_known=np.ones(12).reshape(-1, 1))
+		else:
+			cost = 'abs'
+			if static_fac == 'static':
+				a = 5
+				H_sd, A_sd, T_sd = learn_HAT(case, tensor_copy, a, b, num_iter=2000, lr=0.1, dis=False, cost_function=cost, H_known = H_known_Sd, A_known = A_au, T_known=np.ones(12).reshape(-1, 1))
+			else:
+				a = 2
+				H_sd, A_sd, T_sd = learn_HAT(case, tensor_copy, a, b, num_iter=2000, lr=0.1, dis=False, cost_function=cost, A_known = A_au, T_known=np.ones(12).reshape(-1, 1))
+
+		prediction = multiply_case(H_sd,A_sd,T_sd,case)
+		pred_cell[i][j] = prediction[i][j][k]
 
 
-
-save_obj(pred_cell, "pred_cell_month_" + str(k))
+save_obj(pred_cell, "pred_cell_month_" + static_fac + "_" + algo + "_" + str(k))
