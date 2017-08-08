fe90fc8efcb7f93d03d19095bf8f36920c212f2d
ylwander <yj9xs@virginia.edu>
2017-08-07 20:25:32 -0400

    update

--- aaai/compute_error_script.py
+++ aaai/compute_error_script.py
@@ -21,27 +21,26 @@ MAX_NUM_MY_JOBS = 140
 DELAY_NUM_JOBS_EXCEEDED = 10
 import time
 
-for method  in ['normal']:
-	for algo in ['gd_decay']:
-		for iters in [100]:
-			for static in ['static', 'None']:
-				for lam in [0.001,0.01, 0.1, 0, 1]:
-					OFILE = "%s/out_%s_%f.out" % (SLURM_OUT, static, lam)
-					EFILE = "%s/out_%s_%f.err" % (SLURM_OUT, static, lam)
-					SLURM_SCRIPT = "%s/out_%s_%f.pbs" % ("./pbs_file", static, lam)
-					CMD = 'python compute_error.py %s %f' % (static, lam)
-					lines = []
-					lines.append("#!/bin/sh\n")
-					lines.append('#SBATCH --time=1-16:0:00\n')
-					lines.append('#SBATCH --mem=16\n')
-					lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
-					lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
-					lines.append(CMD + '\n')
-					with open(SLURM_SCRIPT, 'w') as f:
-						f.writelines(lines)
-					command = ['sbatch', SLURM_SCRIPT]
-					while len(delegator.run('squeue -u %s' % username).out.split("\n")) > MAX_NUM_MY_JOBS + 2:
-						time.sleep(DELAY_NUM_JOBS_EXCEEDED)
+for train_iter in [500, 1000, 1500, 2000]:
+	for algo in ['adagrad', 'gd']:
+		for static in ['static', 'None']:
+			for lam in [0.01, 0.1, 0, 1]:
+				OFILE = "%s/out_%d_%s_%s_%f.out" % (SLURM_OUT, train_iter, algo, static, lam)
+				EFILE = "%s/out_%d_%s_%s_%f.err" % (SLURM_OUT, train_iter, algo, static, lam)
+				SLURM_SCRIPT = "%s/out_%d_%s_%s_%f.pbs" % ("./pbs_file", train_iter, algo, static, lam)
+				CMD = 'python compute_error.py %d %s %s %f' % (train_iter, algo, static, lam)
+				lines = []
+				lines.append("#!/bin/sh\n")
+				lines.append('#SBATCH --time=1-16:0:00\n')
+				lines.append('#SBATCH --mem=16\n')
+				lines.append('#SBATCH -o ' + '"' + OFILE + '"\n')
+				lines.append('#SBATCH -e ' + '"' + EFILE + '"\n')
+				lines.append(CMD + '\n')
+				with open(SLURM_SCRIPT, 'w') as f:
+					f.writelines(lines)
+				command = ['sbatch', SLURM_SCRIPT]
+				while len(delegator.run('squeue -u %s' % username).out.split("\n")) > MAX_NUM_MY_JOBS + 2:
+					time.sleep(DELAY_NUM_JOBS_EXCEEDED)
 
-					delegator.run(command, block=False)
-					print SLURM_SCRIPT
+				delegator.run(command, block=False)
+				print SLURM_SCRIPT
