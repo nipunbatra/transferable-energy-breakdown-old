7398f68037434e6a7cfa9eff0da47e06ad576816
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-11 16:49:01 +0530

    minimum 3 factors for Home

--- aaai/sparse-normal-cv.py
+++ aaai/sparse-normal-cv.py
@@ -127,7 +127,7 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(source_df)):
 	best_params_global[outer_loop_iteration] = {}
 	for num_iterations_cv in range(100, 1300, 300):
 		for num_season_factors_cv in range(2, 4):
-			for num_home_factors_cv in range(2, 4):
+			for num_home_factors_cv in range(3, 5):
 				pred_inner = {}
 				for train_inner, test_inner in inner_kf.split(overall_df_inner):
 					train_ix_inner = overall_df_inner.index[train_inner]
