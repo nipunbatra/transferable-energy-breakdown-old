d9c1610d0d7f25d9a3025ba4618967d4373909ae
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-11 14:25:39 +0530

    more changes to the script to comput the MF baseline result

--- aaai/compute-results-normal-mf-1-appliance.py
+++ aaai/compute-results-normal-mf-1-appliance.py
@@ -1,25 +1,32 @@
 from common import  compute_rmse_fraction
 import os
 import pickle
+import pandas as pd
 
 region = "SanDiego"
 target = region
 year = 2014
 APPLIANCES = ['fridge', 'hvac', 'wm', 'mw', 'oven', 'dw']
 
-
-for appliance in APPLIANCES:
-	for features in ['energy', 'energy_static']:
-
-		for cost in ['absolute']:
+cost = 'absolute'
+out = {}
+for features in ['energy', 'energy_static']:
+	out[features] = {}
+	for appliance in APPLIANCES:
+		out[features][appliance] = {}
+		for train_percentage in range(10, 110, 10):
+			out[features][appliance][train_percentage] = {}
 			for random_seed in range(5):
-				for train_percentage in range(10, 110, 10):
-
-
-					name = "{}-{}-{}".format(features, random_seed, train_percentage)
-					directory = os.path.expanduser('~/aaai2017/Normal-MF_{}/{}'.format(target, appliance))
-
+				out[features][appliance][train_percentage][random_seed] = {}
+				name = "{}-{}-{}".format(features, random_seed, train_percentage)
+				directory = os.path.expanduser('~/aaai2017/Normal-MF_{}/{}'.format(target, appliance))
+				try:
 					filename = '{}'.format(os.path.join(directory, name + '.pkl'))
-					out = pickle.load(open(filename, 'r'))
-					pred = out['Prediction']
-
+					p = pickle.load(open(filename, 'r'))
+					pred = p['Prediction']
+					if appliance=="hvac":
+						pred = pred[['hvac_{}'.format(month) for month in range(5, 11)]]
+						out[features][appliance][train_percentage][random_seed] = compute_rmse_fraction(appliance, pred, target)[2]
+				except:
+					print(appliance, features, train_percentage, random_seed)
+			out[features][appliance][train_percentage] = pd.Series(out[features][appliance][train_percentage][random_seed]).mean()
