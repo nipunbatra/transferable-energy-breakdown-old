4b9be360ca510dcba8fc6d13992efeeae00cd7a9
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-13 17:22:12 +0530

    Added cmopute sparse transer rresults

--- aaai/compute-results-sparse-transfer.py
+++ aaai/compute-results-sparse-transfer.py
@@ -11,29 +11,37 @@ appliance_index = {appliance: APPLIANCES_ORDER.index(appliance) for appliance in
 
 import os
 import pickle
+source = 'Austin'
+target = 'SanDiego'
+cost = 'l21'
 out = {}
 for static_fac in ['None','static']:
 	out[static_fac] = {}
-	for lam in [0.001, 0.01, 0.1, 1.]:
+	for lam in [0]:
 		out[static_fac][lam] = {}
-		for num_home_factors in range(3, 9):
-			out[static_fac][lam][num_home_factors] = {}
-			for num_season_factors in range(1, 9):
-				out[static_fac][lam][num_home_factors][num_season_factors] = {}
-				for train_percentage in [x*1. for x in range(10, 110, 10)]:
-					out[static_fac][lam][num_home_factors][num_season_factors][int(train_percentage)] = {}
-					for random_seed in range(5):
-						name = "{}-{}-{}-{}-{}-{}".format(static_fac, lam, num_home_factors, num_season_factors,
-						                                  random_seed, train_percentage)
-						print(name)
-						pr = pickle.load(open(os.path.expanduser('~/aaai2017/transfer/' + name + '.pkl'), 'r'))
-						o = {}
-						for appliance in APPLIANCES_ORDER:
-							if appliance=="hvac":
-								o[appliance] = compute_rmse_fraction(appliance, pr[appliance][range(4, 10)], 'SanDiego')[2]
-							else:
-								o[appliance] = compute_rmse_fraction(appliance, pr[appliance], 'SanDiego')[2]
-						out[static_fac][lam][num_home_factors][num_season_factors][int(train_percentage)][random_seed] = o
+		for train_percentage in range(10, 110, 20):
+
+			out[static_fac][lam][train_percentage] ={}
+			for random_seed in range(5):
+				name = "{}-{}-{}-{}-{}-{}-{}".format(source, target, static_fac, lam, random_seed, train_percentage,
+				                                     cost)
+				directory = os.path.expanduser('~/aaai2017/transfer_{}_{}_{}/'.format(source, target, cost))
+
+				filename = os.path.join(directory, name + '.pkl')
+				try:
+					pr = pickle.load(open(filename, 'r'))
+					pred = pr['Prediction']
+					'''
+										if appliance == "hvac":
+						pred = pred[['hvac_{}'.format(month) for month in range(5, 11)]]
+						out[static_fac][lam][train_percentage] = \
+					compute_rmse_fraction(appliance, pred, target)[2]
+					'''
+
+				except:
+					pass
+				print (name)
+
 
 
 
