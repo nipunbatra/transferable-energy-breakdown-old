b9689052cad4dc0feb808b8a2841fab466d946cc
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-23 13:06:52 +0530

    printing

--- aaai/hcdm-transfer.py
+++ aaai/hcdm-transfer.py
@@ -29,7 +29,7 @@ for random_seed in range(2):
 
 
 		CMD = 'python sparse-transfer-cv-static-matrix.py {} {} {} {} {}'.format(source, target,  random_seed, train_percentage, "&> "+str(random_seed)+"_"+str(train_percentage)+"&")
-
+		print(CMD)
 		command = [CMD]
 
 		delegator.run(command, block=False)
