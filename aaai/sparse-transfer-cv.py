6b5cb9edc36f733f54e786ad8d46a520189bd6af
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-24 19:27:53 +0530

    more checks for transfer CV

--- aaai/sparse-transfer-cv.py
+++ aaai/sparse-transfer-cv.py
@@ -148,9 +148,9 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 	overall_df_inner = target_df.loc[train_ix]
 
 	best_params_global[outer_loop_iteration] = {}
-	for num_iterations_cv in range(100, 1400, 600):
-		for num_season_factors_cv in range(2, 5, 2):
-			for num_home_factors_cv in range(3, 6, 2):
+	for num_iterations_cv in range(100, 1400, 400):
+		for num_season_factors_cv in range(2, 5, 1):
+			for num_home_factors_cv in range(3, 6, 1):
 				pred_inner = {}
 				for train_inner, test_inner in inner_kf.split(overall_df_inner):
 
