801f3128c60416cf4e2d9c28e0910ecdd00b3910
Nipun Batra <nipunb@iiitd.ac.in>
2017-08-25 09:14:21 +0530

    ds

--- aaai/hcdm-transfer-single-region.py
+++ aaai/hcdm-transfer-single-region.py
@@ -21,13 +21,13 @@ MAX_NUM_MY_JOBS = 150
 DELAY_NUM_JOBS_EXCEEDED = 10
 import time
 
-source = 'Boulder'
+source = 'SanDiego'
 target = 'Austin'
 cost = 'l21'
 for static_fac in ['None']:
 	for lam in [0]:
 		for random_seed in range(5):
-			for train_percentage in range(20, 110, 20):
+			for train_percentage in range(10, 110, 10):
 				CMD = 'python sparse-transfer-cv.py {} {} {} {} {} {} {}  &> {}_{}_{}_{}.out &'.format(source, target, static_fac, lam, random_seed, train_percentage, cost, source, target, random_seed, train_percentage)
 				print CMD
 
