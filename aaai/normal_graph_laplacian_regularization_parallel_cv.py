42402029b542d271d4413d9db676724e29eff563
Yiling <yj9xs@virginia.edu>
2017-08-31 14:18:53 -0400

    modify regularization

--- aaai/normal_graph_laplacian_regularization_parallel_cv.py
+++ aaai/normal_graph_laplacian_regularization_parallel_cv.py
@@ -67,7 +67,7 @@ def get_L(X):
 	D = np.diag(np.diag(K))
 	return D - W
 
-def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case):
+def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case, lambda_reg):
     HAT = multiply_case(H, A, T, case)
     mask = ~np.isnan(E_np_masked)
     error_1 = (HAT - E_np_masked)[mask].flatten()
@@ -80,10 +80,10 @@ def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case):
     error_4 = LA.norm(A)
     error_5 = LA.norm(T)
     
-    return np.sqrt((error_1**2).mean()) + lam * error_2 + 0.5 * error_3 + 0.5 * error_4 + 0.5 * error_5
+    return np.sqrt((error_1**2).mean()) + lam * error_2 + lambda_reg * error_3 + lambda_reg * error_4 + lambda_reg * error_5
 
-def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, dis=False, lam = 1, H_known=None,A_known=None, T_known=None, random_seed=0, eps=1e-8, penalty_coeff=0.0):
-
+def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, dis=False, lam=1, H_known=None,
+                            A_known=None, T_known=None, random_seed=0, eps=1e-8, penalty_coeff=0.0, lambda_reg = 0.0):
 	np.random.seed(random_seed)
 	cost = cost_graph_laplacian
 	mg = multigrad(cost, argnums=[0, 1, 2])
@@ -98,7 +98,7 @@ def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, 
 	A_dim = tuple(params[x] for x in A_dim_chars)
 	T_dim_chars = list(cases[case]['HAT'].split(",")[1].split("-")[0].strip())
 	T_dim = tuple(params[x] for x in T_dim_chars)
-	
+
 	H = np.random.rand(*H_dim)
 	A = np.random.rand(*A_dim)
 	T = np.random.rand(*T_dim)
@@ -110,14 +110,14 @@ def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, 
 	Hs = [H.copy()]
 	As = [A.copy()]
 	Ts = [T.copy()]
-	
-	costs = [cost(H, A, T, L, E_np_masked, lam, case)]
+
+	costs = [cost(H, A, T, L, E_np_masked, lam, case, lambda_reg)]
 
 	HATs = [multiply_case(H, A, T, case)]
 
 	# GD procedure
 	for i in range(num_iter):
-		del_h, del_a, del_t = mg(H, A, T, L, E_np_masked, lam, case)
+		del_h, del_a, del_t = mg(H, A, T, L, E_np_masked, lam, case, lambda_reg)
 		sum_square_gradients_H += eps + np.square(del_h)
 		sum_square_gradients_A += eps + np.square(del_a)
 		sum_square_gradients_T += eps + np.square(del_t)
@@ -144,24 +144,24 @@ def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, 
 		As.append(A.copy())
 		Ts.append(T.copy())
 		Hs.append(H.copy())
-		
-		costs.append(cost(H, A, T, L, E_np_masked, lam, case))
-		
+
+		costs.append(cost(H, A, T, L, E_np_masked, lam, case, lambda_reg))
 
 		HATs.append(multiply_case(H, A, T, case))
 		if i % 500 == 0:
 			if dis:
-				print(cost(H, A, T, L, E_np_masked, lam, case))
+				print(cost(H, A, T, L, E_np_masked, lam, case, lambda_reg))
 	return H, A, T, Hs, As, Ts, HATs, costs
-
 
 global case
 global source, source_df, source_dfc, source_tensor, source_static
 global source_L
+global lambda_reg
 case = 2
 
 
-source, random_seed, train_percentage = sys.argv[1:]
+source, lambda_reg, random_seed, train_percentage = sys.argv[1:]
+lambda_reg = float(lambda_reg)
 train_percentage = float(train_percentage)
 random_seed = int(random_seed)
 
@@ -170,7 +170,7 @@ source_df, source_dfc, source_tensor, source_static = create_region_df_dfc_stati
 
 # # using cosine similarity to compute L
 source_L = get_L(source_static)
-#T_degree = np.array(dds[2014][source]).reshape(-1, 1)
+# T_degree = np.array(dds[2014][source]).reshape(-1, 1)
 T_degree = np.ones(12).reshape(-1, 1)
 
 
@@ -193,7 +193,7 @@ def compute_inner_error(overall_df_inner, num_iterations_cv, num_season_factors_
 		H, A, T, Hs, As, Ts, HATs, costs = learn_HAT_adagrad_graph(case, tensor_copy_inner, L_inner, 
 																	num_home_factors_cv, num_season_factors_cv, 
 																	num_iter=num_iterations_cv, lr=1, dis=False, 
-																	lam=lam_cv, T_known = T_degree)
+																	lam=lam_cv, T_known = T_degree, lambda_reg = lambda_reg)
 														
 
 
@@ -336,7 +336,7 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(source_df)):
 	H, A, T, Hs, As, Ts, HATs, costs = learn_HAT_adagrad_graph(case, tensor_copy, L, 
 																best_num_home_factors, best_num_season_factors, 
 																num_iter=best_num_iterations, lr=1, dis=False, 
-																lam=best_lam, T_known = T_degree
+																lam=best_lam, T_known = T_degree, lambda_reg = lambda_reg
 																)
 
 
@@ -352,10 +352,10 @@ for appliance in APPLIANCES_ORDER:
 out = {'Predictions':pred, 'Learning Params':best_params_global}
 
 name = "{}-{}".format(random_seed, train_percentage)
-directory = os.path.expanduser('~/git/pred_graph/regularization/{}/'.format(source))
+directory = os.path.expanduser('~/git/pred_graph/regularization/{}/{}/'.format(source, lambda_reg))
 if not os.path.exists(directory):
 	os.makedirs(directory)
-filename = os.path.expanduser('~/git/pred_graph/regularization/{}/'.format(source)+ name + '.pkl')
+filename = os.path.expanduser('~/git/pred_graph/regularization/{}/{}/'.format(source, lambda_reg)+ name + '.pkl')
 
 if os.path.exists(filename):
 	print("File already exists. Quitting.")
--- aaai/save_A_tf_regularization.py
+++ aaai/save_A_tf_regularization.py
@@ -78,7 +78,7 @@ def get_L(X):
 	return D - W
 
 
-def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case):
+def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case, lambda_reg):
     HAT = multiply_case(H, A, T, case)
     mask = ~np.isnan(E_np_masked)
     error_1 = (HAT - E_np_masked)[mask].flatten()
@@ -91,11 +91,11 @@ def cost_graph_laplacian(H, A, T, L, E_np_masked, lam, case):
     error_4 = LA.norm(A)
     error_5 = LA.norm(T)
     
-    return np.sqrt((error_1**2).mean()) + lam * error_2 + 0.5 * error_3 + 0.5 * error_4 + 0.5 * error_5
+    return np.sqrt((error_1**2).mean()) + lam * error_2 + lambda_reg * error_3 + lambda_reg * error_4 + lambda_reg * error_5
 
 
 def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, dis=False, lam=1, H_known=None,
-                            A_known=None, T_known=None, random_seed=0, eps=1e-8, penalty_coeff=0.0):
+                            A_known=None, T_known=None, random_seed=0, eps=1e-8, penalty_coeff=0.0, lambda_reg = 0.0):
 	np.random.seed(random_seed)
 	cost = cost_graph_laplacian
 	mg = multigrad(cost, argnums=[0, 1, 2])
@@ -123,13 +123,13 @@ def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, 
 	As = [A.copy()]
 	Ts = [T.copy()]
 
-	costs = [cost(H, A, T, L, E_np_masked, lam, case)]
+	costs = [cost(H, A, T, L, E_np_masked, lam, case, lambda_reg)]
 
 	HATs = [multiply_case(H, A, T, case)]
 
 	# GD procedure
 	for i in range(num_iter):
-		del_h, del_a, del_t = mg(H, A, T, L, E_np_masked, lam, case)
+		del_h, del_a, del_t = mg(H, A, T, L, E_np_masked, lam, case, lambda_reg)
 		sum_square_gradients_H += eps + np.square(del_h)
 		sum_square_gradients_A += eps + np.square(del_a)
 		sum_square_gradients_T += eps + np.square(del_t)
@@ -157,18 +157,19 @@ def learn_HAT_adagrad_graph(case, E_np_masked, L, a, b, num_iter=2000, lr=0.01, 
 		Ts.append(T.copy())
 		Hs.append(H.copy())
 
-		costs.append(cost(H, A, T, L, E_np_masked, lam, case))
+		costs.append(cost(H, A, T, L, E_np_masked, lam, case, lambda_reg))
 
 		HATs.append(multiply_case(H, A, T, case))
 		if i % 500 == 0:
 			if dis:
-				print(cost(H, A, T, L, E_np_masked, lam, case))
+				print(cost(H, A, T, L, E_np_masked, lam, case, lambda_reg))
 	return H, A, T, Hs, As, Ts, HATs, costs
 
 
 
 
-source = sys.argv[1]
+source, lambda_reg = sys.argv[1:]
+lambda_reg = float(lambda_reg)
 source_df, source_dfc, source_tensor, source_static = create_region_df_dfc_static(source, year)
 
 # # using cosine similarity to compute L
@@ -182,8 +183,8 @@ algo = 'adagrad'
 cost = 'l21'
 
 #T_degree = np.array(dds[2014]['Austin']).reshape(-1, 1)
-#T_degree = np.ones(12).reshape(-1, 1)
-T_degree = np.c_[np.array(dds[2014]['Austin']).reshape(-1,1), np.ones(12).reshape(-1, 1)]
+T_degree = np.ones(12).reshape(-1, 1)
+# T_degree = np.c_[np.array(dds[2014]['Austin']).reshape(-1,1), np.ones(12).reshape(-1, 1)]
 
 for appliance in APPLIANCES_ORDER:
 	pred[appliance] = []
@@ -206,10 +207,10 @@ for num_season_factors_cv in range(2, 5):
 			                                                                                num_iter=max_num_iterations,
 			                                                                                lr=1, dis=False,
 			                                                                                lam=lam_cv,
-			                                                                                T_known = T_degree
-			                                                                                )
+			                                                                                T_known = T_degree,
+			                                                                                lambda_reg = lambda_reg)
 			for num_iterations in range(100, 1400, 200):
 				A_store[num_season_factors_cv][num_home_factors_cv][lam_cv][num_iterations] = As[num_iterations]
 				print(num_season_factors_cv, num_home_factors_cv, lam_cv, num_iterations)
 
-pickle.dump(A_store, open('predictions/tf_{}_graph_regularization_As.pkl'.format(source), 'w'))
+pickle.dump(A_store, open('predictions/tf_{}_graph_regularization_{}_As.pkl'.format(source, lambda_reg), 'w'))
