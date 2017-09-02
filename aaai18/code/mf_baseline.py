2db83520cfac9f055f2d0c4c57c4074bfadd48c0
Nipun Batra <nipunb@iiitd.ac.in>
2017-09-02 15:14:26 +0530

    Added new code for MFo

--- aaai18/code/mf_baseline.py
+++ aaai18/code/mf_baseline.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import KFold, train_test_split
 
 from common import compute_rmse_fraction
 from mf_core import *
+import pickle
 
 appliance_index = {appliance: APPLIANCES_ORDER.index(appliance) for appliance in APPLIANCES_ORDER}
 
@@ -11,7 +12,7 @@ import sys
 
 n_splits = 10
 
-source, target, appliance, features, random_seed, train_percentage = sys.argv[1:]
+setting, source, target, appliance, features, random_seed, train_percentage = sys.argv[1:]
 random_seed = int(random_seed)
 train_percentage = float(train_percentage)
 
@@ -19,6 +20,9 @@ source_df, source_dfc, source_static_df, source_X_matrix, source_X_normalised, s
 target_df, target_dfc, target_static_df, target_X_matrix, target_X_normalised, target_matrix_max, target_matrix_min, target_appliance_cols, target_aggregate_cols, target_idx_user, target_data_user = create_df_dfc_static(target, 2014, appliance, features)
 
 year = 2014
+
+X_store = pickle.load(open('../predictions/{}-X.pkl'.format(source), 'r'))
+
 
 kf = KFold(n_splits=n_splits)
 
@@ -74,11 +78,27 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 				test_ix_inner = overall_df_inner.index[test_inner]
 				train_test_ix_inner = np.concatenate([test_ix_inner, train_ix_inner])
 				df_t_inner, dfc_t_inner = target_df.loc[train_test_ix_inner], target_dfc.loc[train_test_ix_inner]
+
+
+				if setting == 'transfer':
+					X_source = X_store[appliance][features][num_iterations_cv][num_latent_factors_cv]
+					from collections import OrderedDict
+
+					data_items = OrderedDict()
+					idx_items = OrderedDict()
+
+					for count in range(X_source.shape[0]):
+						data_items['lat_%d' % count] = np.array(X_source[count]).flatten()
+						idx_items['lat_%d' % count] = np.array(range(X_source[count].size))
+				else:
+
+					data_items = None
+					idx_items = None
 				X_matrix, X_normalised, matrix_max, matrix_min, appliance_cols, aggregate_cols = preprocess(df_t_inner,
 				                                                                                            dfc_t_inner,
 				                                                                                            appliance,
-				                                                                                            col_max,
-				                                                                                            col_min,
+				                                                                                            target_matrix_max,
+				                                                                                            target_matrix_min,
 				                                                                                            False)
 
 				static_features = get_static_features(dfc_t_inner, X_normalised)
@@ -94,15 +114,16 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 						idx_user = None
 						data_user =None
 
+
 				A = create_matrix_factorised(appliance, test_ix_inner, X_normalised)
 				X, Y, res = nmf_features(A=A, k=num_latent_factors_cv, constant=0.01, regularisation=False,
 				                         idx_user=idx_user, data_user=data_user,
-				                         idx_item=None, data_item=None, MAX_ITERS=num_iterations_cv, cost=cost)
+				                         idx_item=idx_items, data_item=data_items, MAX_ITERS=num_iterations_cv, cost='absolute')
 
 				pred_inner = []
 
 				pred_inner.append(create_prediction(test_ix_inner, X, Y, X_normalised, appliance,
-				                                    col_max, col_min, appliance_cols))
+				                                    target_matrix_max, target_matrix_min, appliance_cols))
 
 			err = {}
 
@@ -146,8 +167,8 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 	X_matrix, X_normalised, matrix_max, matrix_min, appliance_cols, aggregate_cols = preprocess(df_t,
 	                                                                                            dfc_t,
 	                                                                                            appliance,
-	                                                                                            col_max,
-	                                                                                            col_min,
+	                                                                                            target_matrix_max,
+	                                                                                            target_matrix_min,
 	                                                                                            False)
 
 	static_features = get_static_features(dfc_t, X_normalised)
@@ -162,23 +183,38 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 			idx_user = None
 			data_user = None
 
+	if setting == 'transfer':
+		X_source = X_store[appliance][features][best_num_iterations][best_num_latent_factors]
+		from collections import OrderedDict
+
+		data_items = OrderedDict()
+		idx_items = OrderedDict()
+
+		for count in range(X_source.shape[0]):
+			data_items['lat_%d' % count] = np.array(X_source[count]).flatten()
+			idx_items['lat_%d' % count] = np.array(range(X_source[count].size))
+	else:
+
+		data_items = None
+		idx_items = None
+
 	A = create_matrix_factorised(appliance, test_ix, X_normalised)
 	X, Y, res = nmf_features(A=A, k=best_num_latent_factors, constant=0.01, regularisation=False,
 	                         idx_user=idx_user, data_user=data_user,
-	                         idx_item=None, data_item=None, MAX_ITERS=best_num_iterations, cost=cost)
+	                         idx_item=idx_items, data_item=data_items, MAX_ITERS=best_num_iterations, cost='absolute')
 
 	pred_df.append(create_prediction(test_ix, X, Y, X_normalised, appliance,
-	                                 col_max, col_min, appliance_cols, normalisation=True))
+	                                 target_matrix_max, target_matrix_min, appliance_cols, normalisation=True))
 
 
 out = {'Prediction': pd.concat(pred_df), 'Parameters': best_params_global}
 import os
 import pickle
 name = "{}-{}-{}".format(features, random_seed, train_percentage)
-directory = os.path.expanduser('~/aaai2017/Normal-MF_{}/{}'.format(target, appliance))
+directory = os.path.expanduser('~/git/scalable-nilm/aaai18/predictions/TF/{}/case-{}/{}/{}'.format(setting, case, static_use, constant_use))
 if not os.path.exists(directory):
 	os.makedirs(directory)
-filename = '{}'.format(os.path.join(directory, name + '.pkl'))
+filename = os.path.join(directory, name + '.pkl')
 pickle.dump(out, open(filename,'w'))
 
 
