342870e7849fd8d2f5a312cfee529d077387e478
Nipun Batra <nipunb@iiitd.ac.in>
2017-09-08 13:39:29 +0530

    with logs nowg

--- aaai18/cluster/Server/hcdm_mf.py
+++ aaai18/cluster/Server/hcdm_mf.py
@@ -10,9 +10,17 @@ for appliance in ['hvac','fridge','mw','dw','oven','wm']:
 	for setting in ['transfer', 'normal']:
 		for train_percentage in [6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
 			for random_seed in range(4):
-				CMD = 'python ../../code/mf_baseline.py {} {} {} {} {} {} {} &'.format(setting, source, target, appliance,
+				CMD = 'python ../../code/mf_baseline.py {} {} {} {} {} {} {} &> ../../logs/{}-{}-{}-{}-{}-{}-{}.out &'.format(setting, source, target, appliance,
 			                                                                                                     feature,
 			                                                                                                     random_seed,
-			                                                                                                     train_percentage)
+			                                                                                                     train_percentage,
+			                                                                                                                setting,
+			                                                                                                                source,
+			                                                                                                                target,
+			                                                                                                                appliance,
+			                                                                                                                feature,
+			                                                                                                                random_seed,
+			                                                                                                                train_percentage
+				                                                                                                            )
 				print(CMD)
 
