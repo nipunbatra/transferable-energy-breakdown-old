bf3797576f33418da0ebd506ddf71236f9642aee
Nipun Batra <nipunb@iiitd.ac.in>
2017-09-08 13:27:27 +0530

    Added the name of the appliance also

--- aaai18/cluster/Server/hcdm_mf.py
+++ aaai18/cluster/Server/hcdm_mf.py
@@ -10,7 +10,7 @@ for appliance in ['hvac','fridge','mw','dw','oven','wm']:
 	for setting in ['transfer', 'normal']:
 		for train_percentage in [6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
 			for random_seed in range(4):
-				CMD = 'python ../../code/mf_baseline.py {} {} {} {} {} {} {}'.format(setting, source, target, appliance,
+				CMD = 'python ../../code/mf_baseline.py {} {} {} {} {} {} {} &'.format(setting, source, target, appliance,
 			                                                                                                     feature,
 			                                                                                                     random_seed,
 			                                                                                                     train_percentage)
--- aaai18/code/mf_baseline.py
+++ aaai18/code/mf_baseline.py
@@ -201,7 +201,7 @@ for outer_loop_iteration, (train_max, test) in enumerate(kf.split(target_df)):
 out = {'Prediction': pd.concat(pred_df), 'Parameters': best_params_global}
 import os
 import pickle
-name = "{}-{}-{}".format(features, random_seed, train_percentage)
+name = "{}-{}-{}-{}".format(appliance, features, random_seed, train_percentage)
 directory = os.path.expanduser('~/git/scalable-nilm/aaai18/predictions/MF/{}'.format(setting))
 if not os.path.exists(directory):
 	os.makedirs(directory)
