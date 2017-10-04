98bd8d80b24bc1c8ae33bed34cc6c1f1449b41e3
ylwander <yj9xs@virginia.edu>
2017-10-04 10:38:59 -0400

    add static results

--- aaai18/code/compute_tf_results.py
+++ aaai18/code/compute_tf_results.py
@@ -16,20 +16,20 @@ params = {}
 for case in [2, 4]:
 	out[case] = {}
 	params[case] = {}
-	for constant_use in ['True','False']:
+	for constant_use in ['True']:
 		out[case][constant_use] = {}
 		params[case][constant_use] = {}
 		for static_use in ['True', 'False']:
 			out[case][constant_use][static_use] = {}
 			params[case][constant_use][static_use] = {}
 			print case, constant_use, static_use
-			for setting in ['normal','transfer']:
+			for setting in ['normal']:
 				out[case][constant_use][static_use][setting] = {}
 				params[case][constant_use][static_use][setting] = {}
-				for train_percentage in [0., 6., 8., 10., 15., 20., 30., 40.,50.,60.,70.,80.,90.,100.]:
+				for train_percentage in [6., 8., 10., 15., 20., 30., 40.,50.,60.,70.,80.,90.,100.]:
 					out[case][constant_use][static_use][setting][train_percentage] = {}
 					params[case][constant_use][static_use][setting][train_percentage] = {}
-					for random_seed in range(10):
+					for random_seed in range(5):
 						out[case][constant_use][static_use][setting][train_percentage]
 						params[case][constant_use][static_use][setting][train_percentage]
 						if setting == "transfer":
@@ -38,7 +38,7 @@ for case in [2, 4]:
 							name = "{}-{}-{}".format(target, random_seed, train_percentage)
 
 						directory = os.path.expanduser(
-							'~/git/scalable-nilm/aaai18/predictions/TF-all/{}/case-{}/{}/{}'.format(setting, case, static_use, constant_use))
+							'~/git/scalable-nilm/aaai18/predictions/static/TF-all/{}/case-{}/{}/{}'.format(setting, case, static_use, constant_use))
 						if not os.path.exists(directory):
 							os.makedirs(directory)
 						filename = os.path.join(directory, name + '.pkl')
@@ -63,5 +63,5 @@ for case in [2, 4]:
 							print(e)
 							print("Exception")
 					out[case][constant_use][static_use][setting][train_percentage] = pd.DataFrame(out[case][constant_use][static_use][setting][train_percentage]).mean(axis=1)
-pickle.dump(out, open('../predictions/lr-tf-{}-{}-all.pkl'.format(source, target), 'w'))
-pickle.dump(params, open('../predictions/params-lr-tf-{}-{}-all.pkl'.format(source, target), 'w'))
+pickle.dump(out, open('../predictions/lr-tf-{}-{}-static.pkl'.format(source, target), 'w'))
+pickle.dump(params, open('../predictions/params-lr-tf-{}-{}-static.pkl'.format(source, target), 'w'))
