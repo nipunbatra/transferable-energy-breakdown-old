d741821a044f05c0308655c6d6839441d4886708
ylwander <yj9xs@virginia.edu>
2017-10-13 16:04:22 -0400

    add active

--- aaai18/code/job2.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-for i in 4
-do
-	for j in 6 8 9 10 15 20 30 40 50 60 70 80 90 100
-	do
-		python graph_laplacian_parallel_static.py normal 2 True True SanDiego Austin $i $j 1 13
-	done
-done
